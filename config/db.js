310a30711218793a0ce810440ae480dc1c6b0d82
kameha1011 <omarcitofc2001@gmail.com>
2023-08-14 09:25:47 -0300

    added try/catch on city get controller, erased console log on db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const db_uri = process.env.DB_URI;
-console.log(db_uri);
 mongoose
   .connect(db_uri)
   .then(() => {
--- controllers/city.controller.js
+++ controllers/city.controller.js
@@ -1,25 +1,31 @@
 import City from "../models/City.js";
 const controllers = {
-    getCities: async (req, res) => {
-       const cities = await City.find();
-       res.json(cities).status(200);
-    },
-    createCity: async (req, res) => {
-       try {
-         await City.create(req.body);
-         res.json({message: "city created"}).status(201);
-       } catch (error) {
-        res.json({
-            message: error.message
-        }).status(500);
-       }
-    },
-    updateCity: (req, res) => {
-        
-    },
-    deleteCity: (req, res) => {
-        
+  getCities: async (req, res) => {
+    try {
+      const cities = await City.find();
+      res.json(cities).status(200);
+    } catch (error) {
+      res
+        .json({
+          message: error.message,
+        })
+        .status(500);
     }
+  },
+  createCity: async (req, res) => {
+    try {
+      await City.create(req.body);
+      res.json({ message: "city created" }).status(201);
+    } catch (error) {
+      res
+        .json({
+          message: error.message,
+        })
+        .status(500);
+    }
+  },
+  updateCity: (req, res) => {},
+  deleteCity: (req, res) => {},
 };
 
-export default controllers
+export default controllers;
