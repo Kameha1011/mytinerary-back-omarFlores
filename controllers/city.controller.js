01db0a24383cd41d7369b4d92b8fc112e0fcc441
kameha1011 <omarcitofc2001@gmail.com>
2023-08-16 12:14:45 -0300

    created filter by country,continent and city name with query strings

--- controllers/city.controller.js
+++ controllers/city.controller.js
@@ -2,7 +2,11 @@ import City from "../models/City.js";
 const controllers = {
   getCities: async (req, res) => {
     try {
-      const cities = await City.find();
+      const queries = {};
+      if(req.query.name) queries.name = new RegExp(`^${req.query.name}`, 'i');
+      if(req.query.country) queries.country = req.query.country;
+      if(req.query.continent) queries.continent = req.query.continent;
+      const cities = await City.find(queries);
       if (!cities) return res.status(404).json({ message: "cities not found" });
       res.json(cities).status(200);
     } catch (error) {
