f3e1e6626baad6bad83fe38a1a83cff3d4f183e7
kameha1011 <omarcitofc2001@gmail.com>
2023-08-22 11:47:06 -0300

    added city and country filter

--- controllers/city.controller.js
+++ controllers/city.controller.js
@@ -3,10 +3,19 @@ const controllers = {
   getCities: async (req, res) => {
     try {
       const queries = {};
-      if(req.query.name) queries.name = new RegExp(`^${req.query.name}`, 'i');
-      if(req.query.country) queries.country = req.query.country;
-      if(req.query.continent) queries.continent = req.query.continent;
-      const cities = await City.find(queries);
+      if (req.query.name) {
+        queries.name = new RegExp(`^${req.query.name}`, "i");
+      }
+      if (req.query.country) {
+        queries.country = req.query.country;
+      }
+      if (req.query.continent) {
+        queries.continent = req.query.continent;
+      }
+      if (req.query.skip) {
+        queries.skip = req.query.skip;
+      }
+      const cities = await City.find(queries, {}, { skip: queries.skip });
       if (!cities) return res.status(404).json({ message: "cities not found" });
       res.json(cities).status(200);
     } catch (error) {
