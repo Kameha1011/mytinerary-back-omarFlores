6b08b4bc9b135fead8fc1924ffcd9f7fee662d1b
kameha1011 <omarcitofc2001@gmail.com>
2024-07-02 17:14:26 -0300

    added frontend deploy url to admitted cors urls

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-import 'dotenv/config';
-import './config/db.js';
+import "dotenv/config";
+import "./config/db.js";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import indexRoutes from "./routes/index.routes.js";
-import ExpressError from './utils/ExpressError.js';
+import ExpressError from "./utils/ExpressError.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const ORIGIN =
+  process.env.NODE_ENV === "production"
+    ? "https://mytinerary-back-omarflores.vercel.app"
+    : "http://localhost:5173";
 const corsOptions = {
-    origin: 'http://localhost:5173'
-  }
+  origin: ORIGIN,
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(cors(corsOptions));
-app.use('/api', indexRoutes);
+app.use("/api", indexRoutes);
 
-app.all('*', (req, res, next) => {
-  next(new ExpressError(`Can't find ${req.originalUrl} on this server!`, 404))
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, name } = err
-  if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+  const { statusCode = 500, name } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
   res.status(statusCode).json({
     message: err.message,
-    name
-  })
-})
+    name,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
 
 app.listen(PORT, () => console.log("Server running on port " + PORT));
